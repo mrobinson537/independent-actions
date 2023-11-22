a6879e4e4743c47f866644433a91edbac73f610b
Matt Robinson <mrobinson@logitech.com>
2023-11-22 10:20:32 -0700

    Simplify and make prettier.

--- src/components/flex-cards.js
+++ src/components/flex-cards.js
@@ -1,5 +1,6 @@
 // flex cards component
 import classnames from 'classnames'
+import Image from "next/image";
 
 const FlexCards = ({myHobbies, bordered}) => {
 
@@ -10,7 +11,8 @@ const FlexCards = ({myHobbies, bordered}) => {
       {myHobbies.map((item, x) => {
         return (
           <div className="flex-card" key={x}>
-            <img className="flex-card-img" src="https://picsum.photos/380/200"/>
+            {/*<img className="flex-card-img" src="https://picsum.photos/380/200"/>*/}
+            <Image className={"flex-card-img"} src={item.imgPath} alt={item.name} width={380} height={200} />
             <h4 className="flex-card-title">{item.name}</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
               et dolore magna aliqua.
--- src/components/form/contact-form.js
+++ src/components/form/contact-form.js
@@ -2,9 +2,18 @@
 const Form = () => {
 
   const handleSubmit = (e) => {
-    console.log('handle the submit please')
-    console.log(e)
     e.preventDefault()
+    const form = e.target
+    const formData = new FormData(form)
+    const formJson = Object.fromEntries(formData.entries());
+    // console.log(formJson);
+    const processResults = processForm(formJson)
+    if (typeof processResults === 'object') {
+      console.log('processing failed')
+
+      // next step is to display the error messaging on the form
+    }
+
   }
 
   return (
@@ -12,37 +21,83 @@ const Form = () => {
       <form className={"form"} onSubmit={handleSubmit}>
         <div className={"flex-fields"}>
           <div className={"flex-fields-wrap"}>
-            <label htmlFor="fName">
-              <span>First name</span>
-              <input type="text" name={"fname"} />
+            <label htmlFor="name">
+              <span>Your name</span>
+              <div className={"input-wrap"}>
+                <input type="text" name={"name"} required />
+                <span className="err-msg">asdf</span>
+              </div>
             </label>
           </div>
           <div className={"flex-fields-wrap"}>
-            <label htmlFor="lName">
-              <span>Last name</span>
-              <input type="text" name={"lname"} />
+            <label htmlFor="company">
+              <span>Company</span>
+              <input type="text" name={"company"} />
             </label>
           </div>
         </div>
         <div>
           <label htmlFor="lName">
             <span>Email address</span>
-            <input type="email" name={"email"} />
+            <input type="email" name={"email"} required />
           </label>
         </div>
         <div>
           <label htmlFor="description">
             <span>Description</span>
-            <textarea name="description" id="description" cols="30" rows="10" placeholder="What can I do for you?"></textarea>
+            <textarea name="description" id="description" cols="30" rows="10" placeholder="What can I do for you?" required></textarea>
           </label>
         </div>
         <div>
           <button type={"submit"} name={"submit"}>Submit</button>
-          <p>This doesn't actually do anything yet...</p>
         </div>
       </form>
     </div>
   )
+}
+
+// double check client side inputs
+const processForm = (inputs) => {
+
+  let errFound = false
+  let errObj = {}
+
+  for (const key in inputs) {
+    if (key === 'name') {
+      const namecheck = /^\s*([A-Za-z]{1,}([.,] |[-']| )?)+[A-Za-z]+\.?\s*$/.test(inputs[key])
+      if (!namecheck) {
+        errFound = true
+        errObj[key] = 'Please enter a valid name.'
+      }
+    }
+    if (key === 'company') {
+      const companycheck = /^\s*([A-Za-z]{1,}([.,] |[-']| )?)+[A-Za-z]+\.?\s*$/.test(inputs[key])
+      if (!companycheck) {
+        errFound = true
+        errObj[key] = 'Please enter a valid company name.'
+      }
+    }
+    if (key === 'email') {
+      const emailcheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputs[key])
+      if (!emailcheck) {
+        errFound = true
+        errObj[key] = 'Please enter a valid email address.'
+      }
+    }
+    if (key === 'description') {
+      if (inputs[key].indexOf('<script>') !== -1) {
+        errFound = true
+        errObj[key] = 'Please enter a valid description.'
+      }
+    }
+  }
+
+  console.log(errObj)
+  if (errFound) {
+    return errObj
+  }
+
+  return true;
 }
 
 export default Form
